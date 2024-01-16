60865f82713c371692358b999896a7bd3ea13530
Olivia <124981506+OJxShi@users.noreply.github.com>
2024-01-16 09:48:55 -0500

    Update player_script.py

--- player_script.py
+++ player_script.py
@@ -1,5 +1,6 @@
-import config,animated_sprites_script,collision_script, particle_effects_script,random
+import config,animated_sprites_script,collision_script, particle_effects_script,random,math
 from config import *
+from math import sqrt
 from animated_sprites_script import *
 from collision_script import *
 from particle_effects_script import *
@@ -45,6 +46,7 @@ class Player(AnimatedSprite):
         
         self.x = 640
         self.y = 500
+        self.velx = 0
         self.vely = 0
         self.speed = 18
         self.moving = False
@@ -52,6 +54,9 @@ class Player(AnimatedSprite):
         self.attacking_active = False
         self.direction = "right"
         self.animation_lock = 0
+        self.hp = 100
+        self.max_hp = 100
+        self.immunity = 0
         
         self.set_animation("idle right")
     
@@ -72,6 +77,10 @@ class Player(AnimatedSprite):
                 self.move(self.speed)
             else:
                 self.moving = False
+            if K_h in keys:
+                self.hurt(10)
+            elif K_j in keys:
+                self.hp = 100
             
     def interact(self):
         pass
@@ -84,13 +93,11 @@ class Player(AnimatedSprite):
             
         self.x += x
         self.moving = True
-            
-        self.collidex()
     
     def jump(self):
         if self.grounded:
             self.grounded = False
-            self.vely = 75
+            self.vely = -75
             self.set_animation(f"jump {self.direction}")
     
     def attack_s(self):
@@ -101,6 +108,23 @@ class Player(AnimatedSprite):
         
     def attack_l(self):
         pass
+    
+    def hurt(self,dmg=0,dir="p",type=0,kb=(0,0)):
+        global particle_list
+        if self.immunity <= 0:
+            self.hp -= dmg
+            self.set_animation(f"damage {dir}")
+            self.animation_lock = 5
+            particle_list.append(DamageNumber(self.x+random.randrange(-50,50),self.y+random.randrange(-50,50),dmg))
+            if "u" in dir:
+                self.vely = max(self.vely-kb[1],-75)
+            elif "d" in dir:
+                self.vely = min(self.vely+kb[1],75)
+            if "l" in dir:
+                self.velx -= kb[0]
+            elif "r" in dir:
+                self.velx += kb[0]
+            self.immunity = 10
        
     def align(self):
         self.hitbox.center = self.x,self.y
@@ -109,6 +133,8 @@ class Player(AnimatedSprite):
         self.rect.center = self.x,self.y
 
     def collidex(self): #kid codes worlds worst collision system, asked to leave cs class
+        self.velx = round(self.velx * 0.9,2)
+        self.x += self.velx
         self.align()
         c = self.hitbox.collidelist(collision_list)
         if c != -1:
@@ -119,11 +145,20 @@ class Player(AnimatedSprite):
                 self.x = hit.rect.right + self.hitbox.width/2# + 0.5
             elif right_in and not left_in:
                 self.x = hit.rect.left - self.hitbox.width/2# - 0.5
+        
+        
+        for object in objects_list:
+            if object.__class__ == DamagingObject and self.hitbox.colliderect(object.rect):
+                if self.direction == "left":
+                    self.hurt(random.randint(1,5),"ur",kb=(10,40))
+                elif self.direction == "right":
+                    self.hurt(random.randint(1,5),"ul",kb=(10,40))
+        
         self.align()
         
     def collidey(self):
         self.vely = round(self.vely * 0.9,2)
-        self.y += 30 - self.vely
+        self.y += 30 + self.vely
         self.grounded = False
         self.align()
         c = self.hitbox.collidelist(collision_list)
@@ -134,19 +169,19 @@ class Player(AnimatedSprite):
             if bottom_in and not top_in:
                 self.y = hit.rect.top - self.hitbox.height/2
                 self.grounded = True
-                self.vely = 20
+                self.vely = -20
             elif top_in and not bottom_in:
                 self.y = hit.rect.bottom + self.hitbox.height/2
-                self.vely = 20
+                self.vely = -20
         self.align()      
     
     def update_animation(self):
         global particle_list
         if not self.animation_lock:
             if not self.grounded:
-                if self.vely <= 28:
+                if self.vely >= -28:
                     self.set_animation(f"fall {self.direction}")
-                elif self.vely <= 40:
+                elif self.vely >= -40:
                     if self.current_animation["name"][:4] != "fall":
                         self.set_animation(f"mid {self.direction}")
             elif self.moving:
@@ -158,15 +193,30 @@ class Player(AnimatedSprite):
         AnimatedSprite.update(self)
     
     def update(self):
+        self.collidex()
         self.collidey()
         self.update_animation()
         
         if self.animation_lock > 0: self.animation_lock -= 1
+        self.immunity -= 1
+        
+        if self.hp > self.max_hp: self.hp = self.max_hp
+        elif self.hp < 0: self.hp = 0
 
     def draw(self):
         pygame.draw.rect(screen,(63,200,0),(self.hitbox[0]-camera.x,self.hitbox[1]-camera.y,self.hitbox[2],self.hitbox[3]))
         AnimatedSprite.draw(self)
+        bar_w = 60
+        bar_h = 8
+        n = self.hp/self.max_hp
+        bar_c = (255*(1-n**2),255*(1-(n-1)**2),0)
+        pygame.draw.rect(screen,(0,0,0),(self.x-camera.x-bar_w/2-1,self.y-camera.y-86-bar_h,bar_w+2,bar_h+2))
+        pygame.draw.rect(screen,bar_c,(self.x-camera.x-bar_w/2,self.y-camera.y-85-bar_h,bar_w*(self.hp/self.max_hp),bar_h))
 
 
-player = Player()         
+player = Player()  
+
+
+class PlayerDamaging:
+    pass       
         
