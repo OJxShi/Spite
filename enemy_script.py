67753ec6668241af899c05d3413423cf272197a5
Olivia <124981506+OJxShi@users.noreply.github.com>
2024-01-17 10:09:16 -0500

    added buttons + other stuff i forgot (#3)
    * new interface stuff! just buttons really
    
    other shit like sliders to be added someday (possibly never)
    
    * Add files via upload
    
    * updated menu screen to use new buttons
    
    * god i hope this works

---
+++ enemy_script.py
@@ -0,0 +1,5 @@
+import config,animated_sprites_script,collision_script, particle_effects_script,random
+from config import *
+from animated_sprites_script import *
+from collision_script import *
+from particle_effects_script import *
