578ffa2de600fb6cf3800d3f63202d246f9b79a7
Olivia <124981506+OJxShi@users.noreply.github.com>
2024-01-17 10:05:55 -0500

    updated menu screen to use new buttons

--- game_state.py
+++ game_state.py
@@ -1,6 +1,7 @@
-import pygame, config
+import pygame, config, interface
 from pygame.locals import *
 from config import *
+from interface import *
 
 pygame.init()
 pygame.font.init()
@@ -41,15 +42,12 @@ class MainMenu(GameState):
         self.exiting = False
         self.next_state_stored = "Playing"
         
-        self.button_0_text = self.button_font.render("New Game", True, (255,255,255))
-        self.button_1_text = self.button_font.render("Load Game", True, (255,255,255))
-        self.button_2_text = self.button_font.render("Settings", True, (255,255,255))
-        self.button_3_text = self.button_font.render("Quit", True, (255,255,255))
-
-        self.button_0_rect = pygame.Rect(self.button_text_x[0]-25,285,350,90)
-        self.button_1_rect = pygame.Rect(self.button_text_x[1]-25,385,350,90)
-        self.button_2_rect = pygame.Rect(self.button_text_x[2]-25,485,350,90)
-        self.button_3_rect = pygame.Rect(self.button_text_x[3]-25,585,350,90)
+        self.buttons = [
+            Button(-25,285,350,90,"New Game"),
+            Button(-25,385,350,90,"Load Game"),
+            Button(-25,485,350,90,"Settings"),
+            Button(-25,585,350,90,"Quit")
+        ]
     
     def enter(self):
         self.x = 0
@@ -59,17 +57,19 @@ class MainMenu(GameState):
     def event_handle(self,event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if not self.exiting:
-                if self.button_0_rect.collidepoint(event.pos):
-                    self.next_state_stored = "Playing"
-                    self.exiting = True
-                if self.button_1_rect.collidepoint(event.pos):
-                    #self.next_state_stored = "Save Screen"
-                    self.exiting = True
-                if self.button_2_rect.collidepoint(event.pos):
-                    self.next_state_stored = "Pause"
-                    self.exiting = True
-                if self.button_3_rect.collidepoint(event.pos):
-                    self.next_state = "Quit"
+                for i, button in enumerate(self.buttons):
+                    if button.rect.collidepoint(event.pos):
+                        if button.upon_clicked():
+                            if i == 0:
+                                self.next_state_stored = "Playing"
+                            elif i == 1:
+                                self.next_state_stored = "Save Screen"
+                            elif i == 2:
+                                self.next_state_stored = "Pause"
+                            elif i == 3:
+                                self.next_state_stored = "Quit"
+                            self.exiting = True
+                            
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 self.next_state = "Quit"
@@ -82,7 +82,7 @@ class MainMenu(GameState):
                 self.title_text_x = round((-1)*(self.x-10)**2+64)
             for i in range(len(self.button_text_x)):
                 if self.x <(20+i*5):
-                    self.button_text_x[i] = round((-1.5)*(self.x-(15+i*5))**2+84)
+                    self.buttons[i].rect.x = round((-1.5)*(self.x-(15+i*5))**2+59)
             if self.x < -20:
                 self.next_state = self.next_state_stored
                 
@@ -91,22 +91,16 @@ class MainMenu(GameState):
             self.x += 1
             if self.x < 15:
                 self.title_text_x = round((-1)*(self.x-10)**2+64)
-            for i in range(len(self.button_text_x)):
+            for i, buttons in enumerate(self.buttons):
                 if self.x <(20+i*5):
-                    self.button_text_x[i] = round((-1.5)*(self.x-(15+i*5))**2+84)
-        
-        self.button_0_rect = pygame.Rect(self.button_text_x[0]-25,285,350,90)
-        self.button_1_rect = pygame.Rect(self.button_text_x[1]-25,385,350,90)
-        self.button_2_rect = pygame.Rect(self.button_text_x[2]-25,485,350,90)
-        self.button_3_rect = pygame.Rect(self.button_text_x[3]-25,585,350,90)
+                    self.buttons[i].rect.x = round((-1.5)*(self.x-(15+i*5))**2+59)
     
     def draw(self):
         screen.fill((0,0,0))
         screen.blit(self.title_text,(self.title_text_x,100))
-        screen.blit(self.button_0_text,(self.button_text_x[0],300))
-        screen.blit(self.button_1_text,(self.button_text_x[1],400))
-        screen.blit(self.button_2_text,(self.button_text_x[2],500))
-        screen.blit(self.button_3_text,(self.button_text_x[3],600))      
+        
+        for button in self.buttons:
+            button.draw()    
  
 
 class SaveScreen(GameState):
